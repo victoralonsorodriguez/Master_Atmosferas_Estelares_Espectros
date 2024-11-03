12f0ca4c7caaf5015f330d442cd14f2449524132
Alejandro Cerón Fernández <alecefe16@gmail.com>
2024-11-03 23:52:16 +0100

    Update py_dict_fitting_params.py

--- py_dict_fitting_params.py
+++ py_dict_fitting_params.py
@@ -1,16 +1,27 @@
 
 def fiting_parameters():
 
-    spectrum_smoothing = {'EstrellaProblema1.dat':0.012,
-                        'EstrellaProblema2.dat':0.010
+    spectrum_smoothing = {'calientes':0.012,
+                          'frias':0.010
                         }
     
 
-    spectrum_iterations = {'EstrellaProblema1.dat':5,
-                        'EstrellaProblema2.dat':2
+    spectrum_iterations = {'calientes':5,
+                           'frias':2
                         }
     
-    radial_velocity_threshold = {'EstrellaProblema1.dat':0.8,
-                        'EstrellaProblema2.dat':0.4
-                        }
+    radial_velocity_threshold = {'calientes':0.1,
+                                 'frias':0.6
+                                 }
     return spectrum_smoothing,spectrum_iterations,radial_velocity_threshold
+
+
+def tipos_estrella():
+    
+    calientes = ['EstrellaProblema1.dat',
+                ]
+    
+    frias = ['EstrellaProblema2.dat',
+            ]
+
+    return calientes, frias
